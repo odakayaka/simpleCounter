cf6c96312e66003bbed2638f45d30a332f0bb393
odakayaka <ayaka.odaka@gmail.com>
2016-07-04 17:17:12 +0900

    Initial Commit

---
+++ SimpleCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCounter
+//
+//  Created by 尾高文香 on 2016/07/04.
+//  Copyright © 2016年 com.odakaayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
